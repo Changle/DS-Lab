278892e9e10c4c7f6c0989a9511ed7981525f235
Changle Wang <changlewang@ubuntu.(none)>
2011-09-20 16:21:58 -0400

    implement first part of lab 1

--- lab/lock_server.cc
+++ lab/lock_server.cc
@@ -1,14 +1,18 @@
 // the lock server implementation
 
 #include "lock_server.h"
+#include <pthread.h>
 #include <sstream>
 #include <stdio.h>
+#include <map>
 #include <unistd.h>
 #include <arpa/inet.h>
 
 lock_server::lock_server():
   nacquire (0)
 {
+    pthread_mutex_init(&lock_mutex, NULL);
+    pthread_cond_init(&lock_cv, NULL);
 }
 
 lock_protocol::status
@@ -19,5 +23,45 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
   r = nacquire;
   return ret;
 }
+
+lock_protocol::status
+lock_server::grantLock(int clt, lock_protocol::lockid_t lid, int &r)
+{
+
+    bool isBlocked = false;
+    pthread_mutex_lock(&lock_mutex);
+    if(lockStates.find(lid)==lockStates.end())
+    {
+        this->lockStates[lid]=true;
+        isBlocked = true;
+        pthread_mutex_unlock(&lock_mutex);
+
+    }else 
+    {
+        while(this->lockStates[lid]==true)
+        {
+            pthread_cond_wait(&lock_cv, &lock_mutex);
+        }
+        this->lockStates[lid]=true;
+        pthread_mutex_unlock(&lock_mutex);
+    }
+
+    printf("Grant acquire %ld\n", lid);
+    lock_protocol::status ret = lock_protocol::OK;
+    return ret;
+}
+
+lock_protocol::status
+lock_server::releaseLock(int clt, lock_protocol::lockid_t lid, int &r)
+{
+    assert(lockStates[lid]==true);
+    pthread_mutex_lock(&lock_mutex);
+    this->lockStates[lid]=false;
+    pthread_cond_signal(&lock_cv);
+    pthread_mutex_unlock(&lock_mutex);
+    printf("release %ld\n", lid);
+    return lock_protocol::OK;
+}
+
 
 
--- lab/lock_server.h
+++ lab/lock_server.h
@@ -5,6 +5,8 @@
 #define lock_server_h
 
 #include <string>
+#include <map>
+#include <pthread.h>
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
@@ -13,11 +15,17 @@ class lock_server {
 
  protected:
   int nacquire;
+  std::map<lock_protocol::lockid_t,bool> lockStates;
+  pthread_mutex_t lock_mutex;
+  pthread_cond_t lock_cv;
 
  public:
   lock_server();
-  ~lock_server() {};
+  ~lock_server(){};
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status grantLock(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status releaseLock(int clt, lock_protocol::lockid_t lid, int &);
+
 };
 
 #endif 
